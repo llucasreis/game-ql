a0690007bcbbf47a0c23918cab39737f15603d09
llucasreis <pro.lucasreis@gmail.com>
2021-08-20 15:12:38 -0300

    chore: add initial project structure

---
+++ src/main/java/com/llucasreis/gameql/GameQlApplication.java
@@ -0,0 +1,13 @@
+package com.llucasreis.gameql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llucasreis/gameql/GameQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llucasreis.gameql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
