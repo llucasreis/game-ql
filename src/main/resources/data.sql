8136474f2dd15f51d08e2f35bab883e51075b8dd
llucasreis <pro.lucasreis@gmail.com>
2021-08-20 15:14:46 -0300

    feat: load initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into platforms (name) values ('Playstation 4');
+insert into platforms (name) values ('Playstation 3');
+
+
+insert into games (name, release_year, platform_id) values ('Uncharted 2', 2009, 2);
+insert into games (name, release_year, platform_id) values ('Uncharted 3', 2011, 2);
+insert into games (name, release_year, platform_id) values ('Uncharted 4', 2016, 1);
+insert into games (name, release_year, platform_id) values ('Kingdom Hearts 3', 2020, 1);
